b41b9a07f53b008e6b54accffb83f961c486d925
Bogireddy Sai Prasanna Teja Reddy <37105594+bogireddytejareddy@users.noreply.github.com>
2024-10-17 10:56:39 -0500

    Update main.py

--- models/MOMENT/main.py
+++ models/MOMENT/main.py
@@ -1,1 +1,30 @@
+import torch
+import numpy as np
+import pandas as pd
 
+from momentfm import MOMENTPipeline
+from sklearn.cluster import KMeans
+
+
+def moment(ts, labels, nclust):
+    model = MOMENTPipeline.from_pretrained(
+            "AutonLab/MOMENT-1-large", 
+            model_kwargs={"task_name": "embedding"},
+        )
+    model.init()
+    model.cuda()
+
+    rep = []
+    for t in ts:
+        context = torch.tensor(t).unsqueeze(0).unsqueeze(0).to(torch.float32)
+        embeddings = model(context.cuda())
+        
+        representation = embeddings.embeddings.squeeze(0).detach().cpu().numpy()
+        rep.append(representation)
+        
+    rep = np.array(rep)
+
+    kmeans = KMeans(n_clusters=nclust, init='random', n_init=1).fit(rep)
+    pred = kmeans.labels_
+
+    return pred
