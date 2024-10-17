dd2ea28f6a4e43f91222eb1ad0a4577fb3e8dfa3
Bogireddy Sai Prasanna Teja Reddy <37105594+bogireddytejareddy@users.noreply.github.com>
2024-10-17 11:01:11 -0500

    Update main.py

--- models/CHRONOS/main.py
+++ models/CHRONOS/main.py
@@ -1,1 +1,30 @@
 
+import os
+import time
+import torch
+import numpy as np
+import pandas as pd
+from chronos import ChronosPipeline
+from sklearn.cluster import KMeans
+
+def chronos(ts, labels, nclust):
+    pipeline = ChronosPipeline.from_pretrained(
+            "./chronos-t5-small",
+            device_map="cuda",
+            torch_dtype=torch.bfloat16,
+        )
+
+    rep = []
+    for t in ts:
+        context = torch.tensor(t)
+        embeddings, tokenizer_state = pipeline.embed(context)
+        representation = torch.mean(embeddings, dim=1).to(torch.float32)
+        rep.append(representation.squeeze(dim=0).cpu().detach().numpy())
+        
+    rep = np.array(rep)
+
+    kmeans = KMeans(n_clusters=nclust, init='random', n_init=1).fit(rep)
+    pred = kmeans.labels_
+
+    return pred
+  
